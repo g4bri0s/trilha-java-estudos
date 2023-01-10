26718dfdf6c23329a74c5aa721d1b82ce424af12
g4bri0s <g.cristianods@gmail.com>
2023-01-10 15:42:42 -0300

    Primeiro sistema de SmartTV

---
+++ PrimeiroTeste/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println((smartTv.ligada == true) ? "Ligada" : "Desligada");
+
+        smartTv.aumentarVolume();
+        smartTv.diminuirVolume();
+        smartTv.alimentar();
+
+        System.out.println((smartTv.ligada == true) ? "Ligada" : "Desligada");
+
+    }
+}
