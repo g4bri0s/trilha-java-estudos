f083bd50728aedfe6cd35a4013d71f4b2ebd299f
g4bri0s <g.cristianods@gmail.com>
2023-01-23 00:44:55 -0300

    Criação de cursos e mentorias do bootcamp

--- POO-BootCamp/Main/src/Main.java
+++ POO-BootCamp/Main/src/Main.java
@@ -1,5 +1,18 @@
+import java.time.LocalDate;
+
+import dominio.Curso;
+import dominio.Mentoria;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-    
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Spring Boot");
+        curso1.setDescricao("Descricao do Curso Spring Boot");
+        curso1.setCargaHoraria(5);
+        
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Projeto para portifolio");
+        mentoria1.setDescricao("Descricao do Projeto para portifolio");
+        mentoria1.setData(LocalDate.now());
     }
 }
